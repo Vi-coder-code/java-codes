bd3a928904d42c34e813fbfda5f0de183d796608
Vi-coder-code <vishal.kumar781615@gmail.com>
2025-01-27 16:36:37 +0530

    check

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+
+        // ThIS IS MY FIRST JAVA PROGRAM
+        // I LEARN FIRST USE OF LN \N
+        /*
+        THIS
+         IS
+         A
+         MULTI
+          LINE
+          COMMENT
+         */
+        System.out.println("It's really good!");
+        System.out.println("Bye me pizza!");
+        System.out.println("I WANT TO BYE A PIZZA FROM ZOMATO");
+    }
+}
